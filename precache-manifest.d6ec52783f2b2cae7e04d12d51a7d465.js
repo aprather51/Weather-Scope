66a046c7494a615140d2d91d77e08785b60acb55
Arthur Prather <apratherjr@gmail.com>
2019-04-20 15:09:00 -0500

    Updates

---
+++ precache-manifest.d6ec52783f2b2cae7e04d12d51a7d465.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "24d6906eaef63a937c3038dd2f6f0ee1",
+    "url": "/weather-scope/static/media/weather_scope.24d6906e.png"
+  },
+  {
+    "revision": "8b9bd03a46599514f869",
+    "url": "/weather-scope/static/js/runtime~main.8b9bd03a.js"
+  },
+  {
+    "revision": "b5a5740206ecce6c92ed",
+    "url": "/weather-scope/static/js/main.b5a57402.chunk.js"
+  },
+  {
+    "revision": "7ba567bd5b861d88544a",
+    "url": "/weather-scope/static/js/1.7ba567bd.chunk.js"
+  },
+  {
+    "revision": "b5a5740206ecce6c92ed",
+    "url": "/weather-scope/static/css/main.079e0234.chunk.css"
+  },
+  {
+    "revision": "d3a5e12fd718672b76372f0d1cce8459",
+    "url": "/weather-scope/index.html"
+  }
+];
